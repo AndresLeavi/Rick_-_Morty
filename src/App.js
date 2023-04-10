dedf52b68c3ffda0fd89408dab596f81b0a42342
Andresleavi <andresleavi092@gmail.com>
2023-04-10 12:58:10 -0300

    Ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Nav from './components/Nav'; 
+import { useState } from 'react';
+import axios from 'axios'; 
+
+// const example = {
+//    id: 1,
+//    name: 'Rick Sanchez',
+//    status: 'Alive',
+//    species: 'Human',
+//    gender: 'Male',
+//    origin: {
+//       name: 'Earth (C-137)',
+//       url: 'https://rickandmortyapi.com/api/location/1',
+//    },
+//    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+// };
 
 function App() {
+   const [characters, setCharacters] = useState([]);
+
+   // const onSearch = () => {
+   //    setCharacters([...characters, example]) // Al poner los puntitos, ...characters se copiara lo que tiene example mas lo de contiene caracter.
+
+   // }
+
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then( response => response.data)
+      .then((data) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+   //axios funciona al igual que un $.get, la respesta que recibe de la libreria es data, y poterior a ello comienza a trabajar con eso que le brindo el caracter
+
+   const onClose = (id) => {
+      const charactersFiltered = characters.filter(character => character.id !== Number(id));
+      setCharacters(charactersFiltered)
+   }
+
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => alert(characterID)} />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => alert('Emulamos que se cierra la card')}
-         />
+         <Nav onSearch={onSearch} />
+         <Cards characters={characters} onClose={onClose}/>    
       </div>
    );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export default function SearchBar({onSearch}) {
+   let [id, setId] = useState('');
+
+   const handleChange = (event) => {
+      setId(event.target.value)
+   }
+
+
+
    return (
       <div>
-         <input type='search' />
-         <button onClick={onSearch}>Agregar</button>
+         <input type='search' onChange={handleChange} value={id}/>
+         <button onClick={() => {onSearch(id); setId=('')}}>Agregar</button>
       </div>
    );
 }
+//No podemos pasarle argumentos a onSearch que ser encuentra en la etiqueta button porque se esta ejecutando
+//la forma de solucionarlo es llamando una callback
+// Id es mi estado local, value es lo que me escribe en consola el usuario
